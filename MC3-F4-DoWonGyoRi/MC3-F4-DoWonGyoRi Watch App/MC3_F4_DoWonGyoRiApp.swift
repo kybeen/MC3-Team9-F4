261b2fde747d7e0fc776e9834b141aa2170b0a55
Taerogrammer <hyung54321@naver.com>
2023-07-07 16:01:34 +0900

    xcode 파일 생성

---
+++ MC3-F4-DoWonGyoRi/MC3-F4-DoWonGyoRi Watch App/MC3_F4_DoWonGyoRiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MC3_F4_DoWonGyoRiApp.swift
+//  MC3-F4-DoWonGyoRi Watch App
+//
+//  Created by KimTaeHyung on 2023/07/07.
+//
+
+import SwiftUI
+
+@main
+struct MC3_F4_DoWonGyoRi_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MC3-F4-DoWonGyoRi/MC3-F4-DoWonGyoRi/MC3_F4_DoWonGyoRiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MC3_F4_DoWonGyoRiApp.swift
+//  MC3-F4-DoWonGyoRi
+//
+//  Created by KimTaeHyung on 2023/07/07.
+//
+
+import SwiftUI
+
+@main
+struct MC3_F4_DoWonGyoRiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

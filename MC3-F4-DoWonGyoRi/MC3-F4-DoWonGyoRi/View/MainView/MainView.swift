043202bd81834ac8493e1c9b4d3535a8392447cd
dhk767 <dhk767@gmail.com>
2023-07-10 15:33:37 +0900

    [Add] Tering#3 - MainView View 파일 생성

---
+++ MC3-F4-DoWonGyoRi/MC3-F4-DoWonGyoRi/View/MainView/MainView.swift
@@ -0,0 +1,22 @@
+//
+//  MainView.swift
+//  MC3-F4-DoWonGyoRi
+//
+//  Created by 김동현 on 2023/07/10.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack(spacing: 0) {
+            
+        }
+    }
+}
+
+struct MainView_Provider: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}

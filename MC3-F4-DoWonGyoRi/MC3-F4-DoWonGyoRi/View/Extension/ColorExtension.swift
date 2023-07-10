22fe4787604599f1dc2d1bb4f9f86f3be1d83c5b
dhk767 <dhk767@gmail.com>
2023-07-10 15:29:55 +0900

    [Add] Tering#3 - ColorExtension Color 익스텐션, ColorTheme 구조체 추가

--- MC3-F4-DoWonGyoRi/MC3-F4-DoWonGyoRi/View/Extension/ColorExtension.swift
+++ MC3-F4-DoWonGyoRi/MC3-F4-DoWonGyoRi/View/Extension/ColorExtension.swift
@@ -4,5 +4,28 @@
 //
 //  Created by 김동현 on 2023/07/10.
 //
+import SwiftUI
 
-import Foundation
+extension Color {
+    /// Assets에 추가한 색상 사용하기 편하도록 extenstion 구현
+    /// ```
+    /// Ex)
+    /// Text("Red Color")
+    ///     .foregroundColor(Color.theme.red)
+    /// ```
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let white = Color("TennisGreen")
+    let gray1 = Color("TennisSkyBlue")
+    let gray2 = Color("TennisBlue")
+    let gray3 = Color("TennisBlack")
+    let gray4 = Color("TennisDarkGray")
+    let gray5 = Color("TennisGray")
+    let black = Color("TennisWhite")
+    let green1 = Color("TennisRedPurple")
+    let green2 = Color("TennisPurple")
+    let green3 = Color("TennisLightRed")
+    let red = Color("TennisRed")
+}

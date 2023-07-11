e88a89a5903691a8f182b05a458eb02a34905dc6
dhk767 <dhk767@gmail.com>
2023-07-12 03:45:14 +0900

    [Feat] Tering#3 - 메인뷰 오늘의 스윙 컴포넌트 배치완료(데이터에 따른 변화는 시험해보아야 함)

--- MC3-F4-DoWonGyoRi/MC3-F4-DoWonGyoRi/View/MainView/MainView.swift
+++ MC3-F4-DoWonGyoRi/MC3-F4-DoWonGyoRi/View/MainView/MainView.swift
@@ -23,10 +23,11 @@ let todayBackhandPerfectCount = 100
 let todayPlayTime = 120
 let lastPlayTime = 60
 let todayPerfectCount = 100
-let perfectDifference = todayPerfectCount - yesterdayForehandPerfectCount
+let perfectDifference = 30
 
 struct MainView: View {
     @State private var selectedTab = 0
+    @State var isAnimationEnabled = false
     
     var body: some View {
         
@@ -36,31 +37,21 @@ struct MainView: View {
             
             VStack(spacing: 0) {
                 tabsContainer()
-                
-                
-                TabView(selection: $selectedTab) {
-                    
-                    ScrollView {
-                        ringChartsContainer()
-                        todaySummaryCountainer()
-                    } // end scroll view
-                    .tag(0)
-                    
-                    ScrollView {
-                    }
-                    .tag(1)
-                    
-                }
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+                tabViewContainer()
             }
         }
         .ignoresSafeArea(.all, edges: .bottom)
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                isAnimationEnabled = true
+            }
+        }
     }
 }
 
 
 extension MainView {
+    
     private func containerView() -> some View {
         HStack {
             Button(action: {
@@ -139,6 +130,29 @@ extension MainView {
         .padding(.leading, 27)
     }
     
+    private func tabViewContainer() -> some View {
+        TabView(selection: $selectedTab) {
+            
+            ScrollView {
+                ringChartsContainer()
+                todaySummaryCountainer()
+            } // end scroll view
+            
+            // 아래의 제스처 Modifier를 사용할 경우, 좌우 스와이프가 되지 않음.
+//            .gesture(DragGesture().onChanged { value in
+//                if value.translation.height < 0 {
+//                    isAnimationEnabled = true
+//                }
+//            })
+            .tag(0)
+            ScrollView {
+            }
+            .tag(1)
+            
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+    }
     
     private func scrollContainer() -> some View {
         
@@ -181,15 +195,15 @@ extension MainView {
             Text("하루 요약")
                 .font(.custom("Inter-Bold", size: 24))
                 .frame(maxWidth: .infinity, alignment: .leading)
-            summaryCountBox()
-            summaryCountBox()
+            summaryCountBox("오늘의 포핸드 Perfect 횟수가", "지난번보다")
+            summaryCountBox("오늘의 백핸드 Perfect 횟수가", "지난번보다")
             summaryTimeBox()
         }
         .padding(.leading, 17)
         .padding(.trailing, 17)
     }
     
-    private func summaryCountBox() -> some View {
+    private func summaryCountBox(_ firstLineString: String, _ compareString: String) -> some View {
         ZStack {
             Rectangle()
                 .foregroundColor(.clear)
@@ -198,7 +212,8 @@ extension MainView {
                 .cornerRadius(13)
                 .padding(.top, 14)
             VStack(spacing: 0) {
-                Text("오늘의 포핸드 Perfect 횟수가 지난번보다\n\(abs(perfectDifference))회 \(perfectDifference > 0 ? "늘었습니다" : "줄었습니다").")
+                Text("\(firstLineString) \(compareString)\n\(abs(perfectDifference))회 \(perfectDifference > 0 ? "늘었습니다" : "줄었습니다").")
+                    .font(.custom("Inter-SemiBold", size: 16))
                     .frame(maxWidth: .infinity, alignment: .leading)
                 
                 Rectangle()
@@ -207,8 +222,9 @@ extension MainView {
                     .padding(.top, 10)
                 
                 // TODO: .animation 활용 -> 스크롤이 내려가서 그 부분이 보일 때, 애니메이션이 되어서 막대그래프가 나타나도록 구현
-                horizontalBarGraphContainer(1000, 1500)
-                horizontalBarGraphContainer(500, 1500)
+                horizontalBarGraphContainer(1500, 2000, leftColor: Color(red: 0.24, green: 0.95, blue: 0.23), rightColor: Color(red: 0.23, green: 0.88, blue: 0.95))
+                
+                horizontalBarGraphContainer(500, 2000)
                     
             }
             
@@ -237,7 +253,8 @@ extension MainView {
                     .padding(.top, 10)
                 
                 // TODO: .animation 활용 -> 스크롤이 내려가서 그 부분이 보일 때, 애니메이션이 되어서 막대그래프가 나타나도록 구현
-                horizontalBarGraphContainer(0, 0, true, 1600, 12000)
+                horizontalBarGraphContainer(0, 0, leftColor: Color(red: 0.24, green: 0.95, blue: 0.23), rightColor: Color(red: 0.23, green: 0.88, blue: 0.95), true, 1600, 12000)
+                
                 horizontalBarGraphContainer(0, 0, true, 12000, 1600)
                     
             }
@@ -248,9 +265,11 @@ extension MainView {
     }
     
     
-    private func horizontalBarGraphContainer(_ swingCount: Int, _ totalSwing: Int, _ isTime: Bool = false, _ standardTime: Int = 0, _ comparisonTime: Int = 0) -> some View {
+    private func horizontalBarGraphContainer(_ swingCount: Int, _ totalSwing: Int, leftColor: Color = .white, rightColor: Color = .white, _ isTime: Bool = false, _ standardTime: Int = 0, _ comparisonTime: Int = 0) -> some View {
         let standardTimeHour = standardTime / 60
         let standardTimeMinutes = standardTime % 60
+        
+       
         
         return VStack(spacing: 0) {
             HStack(spacing: 2) {
@@ -271,13 +290,41 @@ extension MainView {
             .frame(maxWidth: .infinity, alignment: .leading)
             
             VStack {
-                Rectangle()
-                    .frame(maxWidth: sizeCalculate(isTime ? CGFloat(abs(standardTime + comparisonTime)) : CGFloat(abs(totalSwing - swingCount)), isTime ? CGFloat(standardTime) : CGFloat(totalSwing)), maxHeight: 23)
-                    .padding(.top, 10)
+                ZStack {
+                    Rectangle()
+                        .foregroundColor(.clear)
+                        .frame(maxWidth: sizeCalculate(isTime ? CGFloat(abs(standardTime + comparisonTime)) : CGFloat(abs(totalSwing - swingCount)), isTime ? CGFloat(standardTime) : CGFloat(totalSwing)), maxHeight: 23)
+                        .background(
+                            LinearGradient(
+                                stops: [
+                                    Gradient.Stop(color: leftColor, location: 0.00),
+                                    Gradient.Stop(color: rightColor, location: 1.00),
+                                ],
+                                startPoint: UnitPoint(x: 1, y: 0.52),
+                                endPoint: UnitPoint(x: -0.06, y: 0.52)
+                            )
+                        )
+                        .cornerRadius(5)
+                        .overlay(
+                            Text("오늘")
+                                .foregroundColor(.black)
+                                .font(.custom("Inter-SemiBold", size: 14))
+                                .frame(maxWidth: .infinity, alignment: .trailing)
+                                .padding(.trailing, 16)
+                                
+                        )
+                        
+                        .scaleEffect(x: isAnimationEnabled ? 1 : 0, y: 1, anchor: .leading)
+                        .animation(.easeOut(duration: 2), value: isAnimationEnabled)
+                        
+                        .padding(.top, 10)
+                        
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
             }
-            .frame(maxWidth: .infinity, alignment: .leading)
         }
         .padding(.top, 10)
+        
         
         func sizeCalculate(_ A: CGFloat, _ B: CGFloat) -> CGFloat {
             

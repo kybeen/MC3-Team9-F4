7e9dae9fce55f96efd32bb146bad61d45a28aa01
Taerogrammer <hyung54321@naver.com>
2023-07-11 17:18:55 +0900

    [Feat]Tering#10-스윙 개수뷰 레이아웃 작성

---
+++ MC3-F4-DoWonGyoRi/MC3-F4-DoWonGyoRi Watch App/View/ReadyView.swift
@@ -0,0 +1,20 @@
+//
+//  ReadyView.swift
+//  MC3-F4-DoWonGyoRi Watch App
+//
+//  Created by KimTaeHyung on 2023/07/11.
+//
+
+import SwiftUI
+
+struct ReadyView: View {
+    var body: some View {
+        Text("Ready View")
+    }
+}
+
+struct ReadyView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReadyView()
+    }
+}

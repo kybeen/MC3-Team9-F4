5e2a01b645879f796a7bc278ddb5fe04d42de6cc
dhk767 <dhk767@gmail.com>
2023-07-12 10:35:13 +0900

    [add] SettingView 파일 추가

---
+++ MC3-F4-DoWonGyoRi/MC3-F4-DoWonGyoRi/View/SettingView/SettingView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingView.swift
+//  MC3-F4-DoWonGyoRi
+//
+//  Created by 김동현 on 2023/07/12.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+struct SettingView_Proviewr: PreviewProvider {
+    static var previews: some View {
+        SettingView()
+    }
+}

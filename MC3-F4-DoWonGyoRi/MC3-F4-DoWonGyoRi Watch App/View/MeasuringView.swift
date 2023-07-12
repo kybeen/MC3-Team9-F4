3f2145af791ec0217236e5f3e9a5bf9d19cf6a42
Taerogrammer <hyung54321@naver.com>
2023-07-13 08:22:07 +0900

    [Feat]#17-1.5초 뒤에 화면 결과 화면 뷰로 이동

--- MC3-F4-DoWonGyoRi/MC3-F4-DoWonGyoRi Watch App/View/MeasuringView.swift
+++ MC3-F4-DoWonGyoRi/MC3-F4-DoWonGyoRi Watch App/View/MeasuringView.swift
@@ -8,10 +8,23 @@
 import SwiftUI
 
 struct MeasuringView: View {
+    @State private var isSwingResultViewPresented = false
+    
     var body: some View {
         VStack {
-            Text("측정 중...")
+            Text("측정 중")
         }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                self.isSwingResultViewPresented = true
+            }
+        }
+        .background(
+            NavigationLink(destination: SwingResultView(), isActive: $isSwingResultViewPresented) {
+                EmptyView()
+            }
+            .hidden()
+        )
     }
 }
 

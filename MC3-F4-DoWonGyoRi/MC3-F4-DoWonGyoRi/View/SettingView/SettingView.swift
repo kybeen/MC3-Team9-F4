a772efdc825fecaeb5422fead13acb193a0cceca
dhk767 <dhk767@gmail.com>
2023-07-12 10:44:28 +0900

    [Feat] Tering#21 - namespceContainer 적용 완료

--- MC3-F4-DoWonGyoRi/MC3-F4-DoWonGyoRi/View/SettingView/SettingView.swift
+++ MC3-F4-DoWonGyoRi/MC3-F4-DoWonGyoRi/View/SettingView/SettingView.swift
@@ -9,12 +9,39 @@ import SwiftUI
 
 struct SettingView: View {
     var body: some View {
-        Text("")
+        VStack(spacing: 0) {
+            namespaceContainer()
+        }
     }
 }
 
 struct SettingView_Proviewr: PreviewProvider {
     static var previews: some View {
         SettingView()
+    }
+}
+
+extension SettingView {
+        
+    
+    private func namespaceContainer() -> some View {
+        VStack(alignment: .center, spacing: 0) {
+            HStack(alignment: .firstTextBaseline, spacing: 0) {
+                Text(title1)
+                    .font(.custom("Inter-Bold", size: 20))
+                    .foregroundColor(Color.theme.teGreen)
+                    .padding(.trailing, 10)
+                Text(title2)
+                    .font(.custom("Inter-Bold", size: 20))
+                    .foregroundColor(Color.theme.teSkyBlue)
+                    
+            }
+            .padding(.bottom, 20)
+            Text(nickname)
+                .font(.custom("Inter-SemiBold", size: 24))
+                .foregroundColor(Color.theme.teWhite)
+            
+        }
+        .frame(maxWidth: UIScreen.main.bounds.width, maxHeight: 150, alignment: .center)
     }
 }

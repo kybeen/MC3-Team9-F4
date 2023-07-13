fc77bb1a3dbab0aaeae81b54a7f40e0ddd19142c
dhk767 <dhk767@gmail.com>
2023-07-13 23:52:24 +0900

    [Feat] #34 - Photo Picker 활용하여 사진 업로드 구현 완료

--- MC3-F4-DoWonGyoRi/MC3-F4-DoWonGyoRi/View/SettingView/UserProfileSettingView.swift
+++ MC3-F4-DoWonGyoRi/MC3-F4-DoWonGyoRi/View/SettingView/UserProfileSettingView.swift
@@ -6,12 +6,15 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct UserProfileSettingView: View {
     
     @State var userNickname = "김배찌"
     @State var userTitle1 = "잘나가는"
     @State var userTitle2 = "세미프로"
+    @State private var selectedItem: PhotosPickerItem? = nil
+    @State private var selectedImageData: Data? = nil
     
     let userTitle1Array = ["지는 법을 모르는", "막을 수 없는", "잘나가는", "절대존엄", "기풍있는"]
     let userTitle2Array = ["아마추어", "슈퍼루키", "세미프로", "월드클래스", "레전드"]
@@ -93,11 +96,30 @@ extension UserProfileSettingView {
             ZStack(alignment: .center) {
                 Circle()
                     .foregroundColor(Color.theme.teGray)
-                Image(systemName: "photo")
-                    .resizable()
-                    .scaledToFit()
-                    .foregroundColor(Color.theme.teWhite)
-                    .frame(width: 45)
+                if let selectedImageData,
+                   let uiImage = UIImage(data: selectedImageData) {
+                    Image(uiImage: uiImage)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(maxWidth: 129, maxHeight: 129)
+                        .cornerRadius(100)
+                    
+                    
+                } else {
+                    if selectedImageData != nil {
+                        Image(uiImage: UIImage(data: selectedImageData!)!)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(maxWidth: 129, maxHeight: 129)
+                            .cornerRadius(100)
+                    } else {
+                        Image(systemName: "photo")
+                            .resizable()
+                            .scaledToFit()
+                            .foregroundColor(Color.theme.teWhite)
+                            .frame(width: 45)
+                    }
+                }
             }
             .frame(maxWidth: 129, maxHeight: 129)
             .padding(.bottom, 32)
@@ -106,22 +128,32 @@ extension UserProfileSettingView {
     }
     
     private func modifyProfileButton() -> some View {
-        NavigationLink(destination: EmptyView()) {
-            Text("사진 변경하기")
-                .font(.custom("Inter-Bold", size: 16))
-                .multilineTextAlignment(.center)
-                .foregroundColor(Color.theme.teGreen)
-                .padding(.horizontal, 28)
-                .padding(.vertical, 9)
-                .cornerRadius(30)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 30)
-                        .inset(by: 0.5)
-                        .stroke(Color.theme.teGreen, lineWidth: 1)
-                )
-        }
-        .padding(.bottom, 15)
-                       
+        PhotosPicker(
+            selection: $selectedItem,
+            matching: .images,
+            photoLibrary: .shared()) {
+                Text("사진 변경하기")
+                    .font(.custom("Inter-Bold", size: 16))
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(Color.theme.teGreen)
+                    .padding(.horizontal, 28)
+                    .padding(.vertical, 9)
+                    .cornerRadius(30)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 30)
+                            .inset(by: 0.5)
+                            .stroke(Color.theme.teGreen, lineWidth: 1)
+                    )
+            }
+            .onChange(of: selectedItem) { newItem in
+                Task {
+                    // Retrieve selected asset in the form of Data
+                    if let data = try? await newItem?.loadTransferable(type: Data.self) {
+                        selectedImageData = data
+                    }
+                }
+            }
+        
     }
 }
 

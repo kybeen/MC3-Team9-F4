03d3bddfccf6154bb4dd6f9aa9947c40a29bed45
Taerogrammer <hyung54321@naver.com>
2023-07-14 01:25:48 +0900

    [Feat]#38-정지 버튼 완성

--- MC3-F4-DoWonGyoRi/MC3-F4-DoWonGyoRi Watch App/View/CountingView.swift
+++ MC3-F4-DoWonGyoRi/MC3-F4-DoWonGyoRi Watch App/View/CountingView.swift
@@ -8,37 +8,39 @@
 import SwiftUI
 
 struct CountingView: View {
-
+    
     var body: some View {
-//        NavigationStack {
-            TabView {
-                ZStack {
-                    Circle()
-                        .frame(width: 150, height: 150, alignment: .center)
-                    VStack {
-                        Text("남은 횟수")
+        TabView {
+            ZStack {
+                Circle()
+                    .frame(width: 150, height: 150, alignment: .center)
+                VStack {
+                    Text("남은 횟수")
+                        .font(.system(size: 16, weight: .bold))
+                        .foregroundColor(Color.black)
+                    Text("50")
+                        .font(.system(size: 56, weight: .bold))
+                        .foregroundColor(Color.black)
+                    
+                    //MARK: - 이 버튼 없으면 정중앙에 정렬됨
+                    NavigationLink(destination: MeasuringView()) {
+                        Text("시작")
                             .font(.system(size: 16, weight: .bold))
                             .foregroundColor(Color.black)
-                        Text("50")
-                            .font(.system(size: 56, weight: .bold))
-                            .foregroundColor(Color.black)
-                        
-                        //MARK: - 이 버튼 없으면 정중앙에 정렬됨
-                        NavigationLink(destination: MeasuringView()) {
-                            Text("시작")
-                                .font(.system(size: 16, weight: .bold))
-                                .foregroundColor(Color.black)
-                        }
                     }
                 }
-                .tabItem{
-                    Image(systemName: "tennisball.fill")
-                        .foregroundColor(Color.watchColor.lightGreen)
-                }
-                .tag(0)
+            }
+            .tabItem{
+                Image(systemName: "tennisball.fill")
+                    .foregroundColor(Color.watchColor.lightGreen)
+            }
+            .tag(0)
+            
+            VStack {
+                Spacer()
                 
-                VStack {
-                    Spacer()
+                //MARK: - 이 버튼 없으면 정중앙에 정렬됨
+                NavigationLink(destination: CompleteView()) {
                     ZStack {
                         Circle()
                             .frame(width: 94, height: 94)
@@ -47,25 +49,21 @@ struct CountingView: View {
                             .frame(width: 38, height: 38, alignment: .center)
                             .foregroundColor(Color.watchColor.black)
                     }
-                    .frame(alignment: .center)
-                    .padding(.bottom, 8)
-                    
-                    Text("종료")
-                        .font(.system(size: 20, weight: .semibold))
-                    //MARK: - 이 버튼 없으면 정중앙에 정렬됨
-                    NavigationLink(destination: CompleteView()) {
-                        Text("스윙완료로 가기")
-                            .font(.system(size: 16, weight: .bold))
-                            .foregroundColor(Color.black)
-                    }
                 }
-                .tabItem{
-                    Image(systemName: "tennisball.fill")
-                        .foregroundColor(Color.watchColor.lightGreen)
-                }
-                .tag(1)
+                .buttonStyle(.borderless)
+                .frame(alignment: .center)
+                .padding(.bottom, 8)
+                
+                Text("종료")
+                    .font(.system(size: 20, weight: .semibold))
+                
             }
-//        }
+            .tabItem{
+                Image(systemName: "tennisball.fill")
+                    .foregroundColor(Color.watchColor.lightGreen)
+            }
+            .tag(1)
+        }
         .navigationBarBackButtonHidden()
     }
 }

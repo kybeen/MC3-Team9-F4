6f2558b64e76f6f66f223980b4cf57531d4413f3
Taerogrammer <hyung54321@naver.com>
2023-07-13 10:02:00 +0900

    [Design]#19-탭바 나눔

--- MC3-F4-DoWonGyoRi/MC3-F4-DoWonGyoRi Watch App/View/ResultView.swift
+++ MC3-F4-DoWonGyoRi/MC3-F4-DoWonGyoRi Watch App/View/ResultView.swift
@@ -9,7 +9,41 @@ import SwiftUI
 
 struct ResultView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            VStack {
+                Text("여기 폭죽 터지고 난리내기")
+            }
+            .tabItem{
+                Image(systemName: "tennisball.fill")
+                    .foregroundColor(Color.watchColor.lightGreen)
+            }
+            .tag(0)
+            
+            VStack {
+                Text("PERFECT, BAD 개수")
+                    .font(.system(size: 20, weight: .semibold))
+            }
+            .tabItem{
+                Image(systemName: "tennisball.fill")
+                    .foregroundColor(Color.watchColor.lightGreen)
+            }
+            .tag(1)
+            
+            VStack {
+                Text("운동 요약")
+                //MARK: - 이 버튼 없으면 정중앙에 정렬됨
+                NavigationLink(destination: CompleteView()) {
+                    Text("완료 버튼")
+                        .font(.system(size: 16, weight: .bold))
+                        .foregroundColor(Color.black)
+                }
+            }
+            .tabItem{
+                Image(systemName: "tennisball.fill")
+                    .foregroundColor(Color.watchColor.lightGreen)
+            }
+            .tag(2)
+        }
     }
 }
 

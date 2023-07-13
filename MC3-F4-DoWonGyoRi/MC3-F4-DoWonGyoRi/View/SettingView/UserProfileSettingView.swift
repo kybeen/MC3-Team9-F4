d6a4cdc6760e41acffd8b792020612f3ae1275c3
dhk767 <dhk767@gmail.com>
2023-07-13 22:54:30 +0900

    [Add] #34 - UserProfileSettingView 파일 생성

---
+++ MC3-F4-DoWonGyoRi/MC3-F4-DoWonGyoRi/View/SettingView/UserProfileSettingView.swift
@@ -0,0 +1,20 @@
+//
+//  UserProfileSettingView.swift
+//  MC3-F4-DoWonGyoRi
+//
+//  Created by 김동현 on 2023/07/13.
+//
+
+import SwiftUI
+
+struct UserProfileSettingView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+struct UserProfileSettingView_Preview: PreviewProvider {
+    static var previews: some View {
+        UserProfileSettingView()
+    }
+}

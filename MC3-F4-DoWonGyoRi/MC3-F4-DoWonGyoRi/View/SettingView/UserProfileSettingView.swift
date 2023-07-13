c3fa7054864089c3a51259410f27630baef6a817
dhk767 <dhk767@gmail.com>
2023-07-13 23:35:51 +0900

    [Feat] #34 - UserProfileSettingView 기본 레이아웃 구현 완료

--- MC3-F4-DoWonGyoRi/MC3-F4-DoWonGyoRi/View/SettingView/UserProfileSettingView.swift
+++ MC3-F4-DoWonGyoRi/MC3-F4-DoWonGyoRi/View/SettingView/UserProfileSettingView.swift
@@ -8,8 +8,120 @@
 import SwiftUI
 
 struct UserProfileSettingView: View {
+    
+    @State var userNickname = "김배찌"
+    @State var userTitle1 = "잘나가는"
+    @State var userTitle2 = "세미프로"
+    
+    let userTitle1Array = ["지는 법을 모르는", "막을 수 없는", "잘나가는", "절대존엄", "기풍있는"]
+    let userTitle2Array = ["아마추어", "슈퍼루키", "세미프로", "월드클래스", "레전드"]
+    
     var body: some View {
-        Text("")
+        VStack(spacing: 0) {
+            Spacer()
+            VStack(spacing: 0) {
+                profilePhotoContainer()
+                modifyProfileButton()
+            }
+            
+            List {
+                Section(content: {
+                    Button(action: {
+                        
+                    }) {
+                        Text(userNickname)
+                            .font(.custom("Inter-Medium", size: 16))
+                            .foregroundColor(.gray)
+                    }
+                    .frame(maxHeight: 60)
+                }, header: {
+                    Text("닉네임")
+                        .font(.custom("Inter-Medium", size: 16))
+                        .foregroundColor(Color.theme.teWhite)
+                        .padding(.bottom, 15)
+                })
+                
+            }
+            .frame(maxHeight: 185)
+            VStack(spacing: 0) {
+                Text("칭호")
+                    .font(.custom("Inter-Medium", size: 16))
+                    .foregroundColor(Color.theme.teWhite)
+                    .frame(maxWidth: UIScreen.main.bounds.width, alignment: .leading)
+                    .padding(.bottom, 17)
+                    .padding(.leading, 18)
+                
+                HStack(spacing: 0) {
+                    Picker("타이틀1", selection: $userTitle1, content: {
+                        ForEach(userTitle1Array, id: \.self) {
+                            Text("\($0)")
+                                .font(.custom("Inter-Medium", size: 16))
+                                .foregroundColor(Color.theme.teWhite)
+                        }
+                    })
+                    .presentationDetents([.fraction(0.4)])
+                    .pickerStyle(.wheel)
+                    
+                    Picker("타이틀2", selection: $userTitle2, content: {
+                        ForEach(userTitle2Array, id: \.self) {
+                            Text("\($0)")
+                                .font(.custom("Inter-Medium", size: 16))
+                                .foregroundColor(Color.theme.teWhite)
+                                
+                        }
+                    })
+                    .frame(alignment: .leading)
+                    .presentationDetents([.fraction(0.4)])
+                    .pickerStyle(.wheel)
+                }
+                
+                .background(Color.theme.teDarkGray)
+                .cornerRadius(20)
+            }
+            .padding(.horizontal, 18)
+            .frame(maxHeight: 210)
+            Spacer()
+        }
+    }
+}
+
+extension UserProfileSettingView {
+    private func profilePhotoContainer() -> some View {
+        Button(action: {
+            
+        }) {
+            ZStack(alignment: .center) {
+                Circle()
+                    .foregroundColor(Color.theme.teGray)
+                Image(systemName: "photo")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundColor(Color.theme.teWhite)
+                    .frame(width: 45)
+            }
+            .frame(maxWidth: 129, maxHeight: 129)
+            .padding(.bottom, 32)
+        }
+        .disabled(true)
+    }
+    
+    private func modifyProfileButton() -> some View {
+        NavigationLink(destination: EmptyView()) {
+            Text("사진 변경하기")
+                .font(.custom("Inter-Bold", size: 16))
+                .multilineTextAlignment(.center)
+                .foregroundColor(Color.theme.teGreen)
+                .padding(.horizontal, 28)
+                .padding(.vertical, 9)
+                .cornerRadius(30)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 30)
+                        .inset(by: 0.5)
+                        .stroke(Color.theme.teGreen, lineWidth: 1)
+                )
+        }
+        .padding(.bottom, 15)
+                       
     }
 }
 

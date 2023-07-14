14e1ee2717f4e351e6da925f70c7b75a6eb51af1
dhk767 <dhk767@gmail.com>
2023-07-14 09:41:21 +0900

    [Add] GuideView 파일 추가

---
+++ MC3-F4-DoWonGyoRi/MC3-F4-DoWonGyoRi/View/GuideView/GuideView.swift
@@ -0,0 +1,20 @@
+//
+//  GuideView.swift
+//  MC3-F4-DoWonGyoRi
+//
+//  Created by 김동현 on 2023/07/14.
+//
+
+import SwiftUI
+
+struct GuideView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+struct GuideView_Preview: PreviewProvider {
+    static var previews: some View {
+        GuideView()
+    }
+}

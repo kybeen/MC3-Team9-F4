d646f89bbaf9823d9dcd14fa03089518546e989d
dhk767 <dhk767@gmail.com>
2023-07-14 11:45:52 +0900

    [Add] #48 - ShareView 파일 추가

---
+++ MC3-F4-DoWonGyoRi/MC3-F4-DoWonGyoRi/View/ShareView/ShareView.swift
@@ -0,0 +1,20 @@
+//
+//  ShareView.swift
+//  MC3-F4-DoWonGyoRi
+//
+//  Created by 김동현 on 2023/07/14.
+//
+
+import SwiftUI
+
+struct ShareView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+struct ShareView_Preview: PreviewProvider {
+    static var previews: some View {
+        ShareView()
+    }
+}

37be5eb0f943e6a68b1b933d6087e853837e5aa0
dhk767 <dhk767@gmail.com>
2023-07-14 15:18:10 +0900

    [Add] OnboardingView 파일 추가

---
+++ MC3-F4-DoWonGyoRi/MC3-F4-DoWonGyoRi/View/OnboardingView/OnboardingView.swift
@@ -0,0 +1,24 @@
+//
+//  OnboardingView.swift
+//  MC3-F4-DoWonGyoRi
+//
+//  Created by 김동현 on 2023/07/14.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+struct OnboardingView_Previewer: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
+
+extension OnboardingView {
+    
+}

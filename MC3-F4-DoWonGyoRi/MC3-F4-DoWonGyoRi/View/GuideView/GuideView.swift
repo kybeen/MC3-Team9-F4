0675ee50e591ead3df37de8291974be2235fe0fd
dhk767 <dhk767@gmail.com>
2023-07-14 10:11:11 +0900

    [Feat] #47 - 가이드뷰 상단 텍스트, 버튼 구현 완료

--- MC3-F4-DoWonGyoRi/MC3-F4-DoWonGyoRi/View/GuideView/GuideView.swift
+++ MC3-F4-DoWonGyoRi/MC3-F4-DoWonGyoRi/View/GuideView/GuideView.swift
@@ -8,9 +8,47 @@
 import SwiftUI
 
 struct GuideView: View {
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        Text("")
+        ZStack {
+            Color.primary.edgesIgnoringSafeArea(.all)
+            VStack(spacing: 0) {
+                HStack(spacing: 0) {
+                    Spacer()
+                    Button(action: {
+                        dismiss()
+                    }) {
+                        Image(systemName: "xmark")
+                            .resizable()
+                            .scaledToFit()
+                            .foregroundColor(Color.theme.teBlack)
+                            .frame(maxWidth: 17)
+                    }
+                }
+                .frame(maxWidth: UIScreen.main.bounds.width)
+                .padding(.horizontal, 36)
+                .padding(.bottom, 30)
+                VStack(spacing: 0) {
+                    Text("테니스 가이드")
+                        .font(.custom("Inter-Medium", size: 20))
+                        .foregroundColor(Color.theme.teGreen)
+                        .padding(.bottom, 6)
+                    Rectangle()
+                        .frame(width: 138, height: 3)
+                        .foregroundColor(Color.theme.teGreen)
+                }
+                .frame(maxWidth: UIScreen.main.bounds.width, alignment: .leading)
+                .padding(.horizontal, 30)
+                
+                ScrollView {
+                    
+                }
+            }
+        }
+        
     }
+    
 }
 
 struct GuideView_Preview: PreviewProvider {

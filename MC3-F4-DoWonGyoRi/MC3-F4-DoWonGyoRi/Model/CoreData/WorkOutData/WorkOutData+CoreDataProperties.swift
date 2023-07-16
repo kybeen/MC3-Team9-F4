3cbe0f5c97ee863bcb048f6ea5684d49caa3a509
dhk767 <dhk767@gmail.com>
2023-07-17 00:50:25 +0900

    [Feat] #64 - WorkoutData 속성 수정, CoredataManager 구현

--- MC3-F4-DoWonGyoRi/MC3-F4-DoWonGyoRi/Model/CoreData/WorkOutData/WorkOutData+CoreDataProperties.swift
+++ MC3-F4-DoWonGyoRi/MC3-F4-DoWonGyoRi/Model/CoreData/WorkOutData/WorkOutData+CoreDataProperties.swift
@@ -16,11 +16,13 @@ extension WorkOutData {
         return NSFetchRequest<WorkOutData>(entityName: "WorkOutData")
     }
 
-    @NSManaged public var isBackhand: Bool
-    @NSManaged public var totalSwingCount: Int16
-    @NSManaged public var perfectSwingCount: Int16
-    @NSManaged public var workoutTime: Int16
     @NSManaged public var burningCalories: Int16
+    @NSManaged public var isBackhand: Bool
+    @NSManaged public var perfectSwingCount: Int16
+    @NSManaged public var totalSwingCount: Int16
+    @NSManaged public var workoutTime: Int16
+    @NSManaged public var id: Int16
+    @NSManaged public var workoutDate: Date?
 
 }
 

1c155e1f6dc57714d786f07cc22e6d18fdc75ca3
Taerogrammer <hyung54321@naver.com>
2023-07-25 11:52:24 +0900

    [Feat]#83-새 폴더 생성

---
+++ MC3_Tering/MC3_Tering/MC3_TeringApp.swift
@@ -0,0 +1,17 @@
+//
+//  MC3_TeringApp.swift
+//  MC3_Tering
+//
+//  Created by KimTaeHyung on 2023/07/25.
+//
+
+import SwiftUI
+
+@main
+struct MC3_TeringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MC3_Tering/MC3_Tering_Watch Watch App/MC3_Tering_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MC3_Tering_WatchApp.swift
+//  MC3_Tering_Watch Watch App
+//
+//  Created by KimTaeHyung on 2023/07/25.
+//
+
+import SwiftUI
+
+@main
+struct MC3_Tering_Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

cd866cccb5f664693bb75587ae1407618d74e7df
Taerogrammer <hyung54321@naver.com>
2023-07-28 16:21:00 +0900

    [Feat]#104-목표 종료 여부 완성

--- MC3_Tering/MC3_Tering_Watch Watch App/View/SwingCompleteView.swift
+++ MC3_Tering/MC3_Tering_Watch Watch App/View/SwingCompleteView.swift
@@ -8,9 +8,43 @@
 import SwiftUI
 
 struct SwingCompleteView: View {
+    
+    @State var progressValue: Float = 0.0
+    @State var countValue: String = "달성완료!"
+    @State var fontSize: CGFloat = 24.0
+    @State private var isSelectViewPresented = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            TimeCircleProgressBar(progress: self.$progressValue, count: self.$countValue, fontSize: self.$fontSize)
+        }
+        .onAppear {
+            progressComplete()
+        }
+        .background(
+            NavigationLink(destination: SelectView(),
+                           isActive: $isSelectViewPresented) {
+                EmptyView()
+            }
+            .hidden()
+        )
+
+        .navigationBarBackButtonHidden()
     }
+}
+
+extension SwingCompleteView {
+    
+    private func progressComplete() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            progressValue = 1
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+            isSelectViewPresented = true
+        }
+    }
+    
+    
 }
 
 struct SwingCompleteView_Previews: PreviewProvider {

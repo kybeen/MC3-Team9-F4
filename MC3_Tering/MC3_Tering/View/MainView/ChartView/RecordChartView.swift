f3db1f3c7224c551a435b77e12ac8dd4bc3acfd6
kybeen <rei980627@gmail.com>
2023-07-31 16:09:38 +0900

    [Add] #129 - WeekChartView 더미데이터 추가

--- MC3_Tering/MC3_Tering/View/MainView/ChartView/RecordChartView.swift
+++ MC3_Tering/MC3_Tering/View/MainView/ChartView/RecordChartView.swift
@@ -7,6 +7,13 @@
 
 import SwiftUI
 
+enum Period {
+    case week
+    case month
+    case sixMonths
+    case year
+}
+
 struct RecordChartView: View {
     @State private var selectedPeriodIndex = 0
     private var periodSelections = ["1주", "1개월", "6개월", "1년"]
@@ -15,16 +22,18 @@ struct RecordChartView: View {
         VStack {
             CustomSegmentedView($selectedPeriodIndex, selections: periodSelections)
             
-//            switch selectedPeriod {
-//            case .week:
-//                WeekChartview()
-//            case .month:
-//                MonthChartView()
-//            case .sixMonths:
-//                SixMonthsChartView()
-//            case .year:
-//                YearChartView()
-//            }
+            switch selectedPeriodIndex {
+            case 0:
+                WeekChartview()
+            case 1:
+                MonthChartView()
+            case 2:
+                SixMonthsChartView()
+            case 3:
+                YearChartView()
+            default:
+                Text("차트를 불러오지 못했습니다.")
+            }
         }
     }
 }
--- MC3_Tering/MC3_Tering/View/MainView/ChartView/WeekChartview.swift
+++ MC3_Tering/MC3_Tering/View/MainView/ChartView/WeekChartview.swift
@@ -5,7 +5,43 @@
 //  Created by 김영빈 on 2023/07/31.
 //
 
+import Charts
 import SwiftUI
+
+struct WeekSwingData: Identifiable {
+    let weekday: String
+    let count: Int
+
+    var id: UUID = UUID()
+}
+struct WeekPerSwingDataType: Identifiable {
+    let swingDataType: String
+    let data: [WeekSwingData]
+
+    var id: UUID = UUID()
+}
+let totalData: [WeekSwingData] = [
+    .init(weekday: Date().date(2023,7,24).getWeekday(), count: 250),
+    .init(weekday: Date().date(2023,7,25).getWeekday(), count: 124),
+    .init(weekday: Date().date(2023,7,26).getWeekday(), count: 135),
+    .init(weekday: Date().date(2023,7,27).getWeekday(), count: 104),
+    .init(weekday: Date().date(2023,7,28).getWeekday(), count: 140),
+    .init(weekday: Date().date(2023,7,29).getWeekday(), count: 90),
+    .init(weekday: Date().date(2023,7,30).getWeekday(), count: 110),
+]
+let perfectData: [WeekSwingData] = [
+    .init(weekday: Date().date(2023,7,24).getWeekday(), count: 200),
+    .init(weekday: Date().date(2023,7,25).getWeekday(), count: 100),
+    .init(weekday: Date().date(2023,7,26).getWeekday(), count: 110),
+    .init(weekday: Date().date(2023,7,27).getWeekday(), count: 90),
+    .init(weekday: Date().date(2023,7,28).getWeekday(), count: 80),
+    .init(weekday: Date().date(2023,7,29).getWeekday(), count: 70),
+    .init(weekday: Date().date(2023,7,30).getWeekday(), count: 100),
+]
+let weekPerSwingDataType: [WeekPerSwingDataType] = [
+    .init(swingDataType: "전체 스윙 횟수", data: totalData),
+    .init(swingDataType: "퍼펙트 스윙 횟수", data: perfectData)
+]
 
 struct WeekChartview: View {
     var body: some View {
